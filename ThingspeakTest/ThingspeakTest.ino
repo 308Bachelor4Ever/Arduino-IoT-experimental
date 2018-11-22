34855f935a7f48ed2b98c779be5604a2deb2d4c3
Daniel Pettersen <dtpett16@student.uia.no>
2018-11-22 12:40:59 +0100

    Added more test projects

---
+++ ThingspeakTest/ThingspeakTest.ino
@@ -0,0 +1,105 @@
+/*
+  Based on WebClientSSL example by Bportaluri.
+  http://kjll.cm/arduino-wifiesp
+*/
+
+// Include the libraries
+#include <SoftwareSerial.h>
+#include <WiFiEspClient.h>
+#include "WiFiEsp.h"
+
+
+// Declare and initialise global arrays for WiFi settings
+char ssid[] = "shiwan";
+char pass[] = "shiwan11";
+
+// Set radio status
+int status = WL_IDLE_STATUS;
+
+// Declare and initialise global variables for Thingspeak connection
+const char server[] = "thingspeak.com";
+const char thingspeakAPIKey[] = "JRFRSDLZYFM3DI8V";
+long postingInterval = 30000;
+
+// Declare global variable for timing
+long lastConnectionTime;
+
+// Declare and initialise data variable
+long myData = 0;
+
+// Create client object
+WiFiESPClient client;
+
+// Connect WiFi module object on GPIO pin 6 (RX) and 7 (TX)
+SoftwareSerial Serial1(6, 7);
+
+void setup() {
+
+  // Initialize serial for debugging
+  Serial.begin(115200);
+
+  // Initialize serial for ESP module
+  Serial1.begin(96000);
+
+  // Initialize ESP module
+  WiFi.init(&Serial1);
+
+  // Check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // Don't continue
+    while(true);
+  }
+
+// Attempt to connect to WiFi network
+while ( status != WL_CONNECTED) {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+
+  // Connect to WPA/WPA2 network
+  status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+}
+
+void loop() {
+  // Get connection info in Serial monitor
+  while(client.available()){
+    char c = client.read();
+    Serial.write(c);
+  }
+  if (millis() - lastConnectionTime > postingInterval) {
+    myData = myData + random(-10, 10);
+    sendThingspeak(myData);
+    lastConnectionTime = millis();
+  }
+}
+
+
+void sendThingspeak(long value) {
+  if (client.connectSSL(server, 443)) {
+    Serial.println("Connected to server");
+    client.println("GET /update*?api_key=" + String(thingspeakAPIKey) +
+    "&field1=" + String(value) + " HTTP/1.1");
+    client.println("Host: api.thingspeak.com");
+    client.println("Connection: close");
+    client.println();
+    Serial.println("Sent to server");
+    }
+  }
+
+  void printWifiStatus() {
+
+    // Print the SSID of the network
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    //Print the Ip adress
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+  }
+  }
+}

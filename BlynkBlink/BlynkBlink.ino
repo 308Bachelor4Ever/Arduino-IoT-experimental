f2fdbaf01928a9943a5eb29a625265e35fc79662
Daniel Pettersen <dtpett16@student.uia.no>
2018-11-23 14:05:58 +0100

    New code for LED controlled over internet with Blynk.
    .DS_Store is also deleted. Will be added to .gitignore

--- BlynkBlink/BlynkBlink.ino
+++ BlynkBlink/BlynkBlink.ino
@@ -21,21 +21,23 @@
     It's very tricky to get it working. Please read this article:
     http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
 
-  Blynk using a LED widget on your phone!
+  You’ll need:
+   - Blynk App (download from AppStore or Google Play)
+   - Arduino Uno board
+   - Decide how to connect to Blynk
+     (USB, Ethernet, Wi-Fi, Bluetooth, ...)
 
-  App project setup:
-    LED widget on V1
+  There is a bunch of great example sketches included to show you how to get
+  started. Think of them as LEGO bricks  and combine them as you wish.
+  For example, take the Ethernet Shield sketch and combine it with the
+  Servo example, or choose a USB sketch and add a code from SendData
+  example.
  *************************************************************/
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 
-#include <WiFiEspServer.h>
-#include <WiFiEspUdp.h>
-#include <WiFiEspClient.h>
-#include <WiFiEsp.h>
-#include <SoftwareSerial.h>
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 
@@ -48,84 +50,35 @@ char auth[] = "b60f347dc3744b63b0b95b3e0a025fb5";
 char ssid[] = "shiwan";
 char pass[] = "shiwan11";
 
-// Declare and intialise variable for radio status
-int status = WL_IDLE_STATUS;
 
 // or Software Serial on Uno, Nano...
-SoftwareSerial Serial1(6, 7); // RX, TX
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(6, 7); // RX, TX
 
-ESP8266 wifi(&Serial1);
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
 
-WidgetLED led1(V1);
-
-BlynkTimer timer;
-
-// V1 LED Widget is blinking
-void blinkLedWidget()
-{
-  if (led1.getValue()) {
-    led1.off();
-    Serial.println("LED on V1: off");
-  } else {
-    led1.on();
-    Serial.println("LED on V1: on");
-  }
-}
+ESP8266 wifi(&EspSerial);
 
 void setup()
 {
-   pinMode(13, OUTPUT);
+  // Debug console
+  Serial.begin(115200);
 
-  // Initialize serial for debugging
-  Serial.begin (115200);
-
-  // Intialize serial for ESP module
-  Serial1.begin(9600);
-
-  // Initialize ESP module
-WiFi.init(&Serial1);
-
-//Check for the presences of the shield
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-
-                   // Don't continue
-                   while (true);
-                 }
-
-                   // Attempt to connect to WiFi netwok
-                   while (status != WL_CONNECTED) {
-                   Serial.print("Attempting to connect to SSID: ");
-                   Serial.println(ssid);
-
-                   //Connect to WPA/WPA2 network
-                   status = WiFi.begin(ssid, pass);
-                 }
-
-                   Serial.println("You're connected to the network");
-printWifiStatus();
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
 
   Blynk.begin(auth, wifi, ssid, pass);
   // You can also specify server:
   //Blynk.begin(auth, wifi, ssid, pass, "blynk-cloud.com", 80);
   //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  timer.setInterval(1000L, blinkLedWidget);
 }
 
 void loop()
 {
   Blynk.run();
-  timer.run();
-}
-
-void printWifiStatus() {
-// Print the SSID of the network
-Serial.print("SSID: ");
-Serial.println(WiFi.SSID());
-
-// Print the IP address
-IPAddress ip = WiFi.localIP();
-Serial.print("IP Address: ");
-Serial.println(ip);
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
 }
